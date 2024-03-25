e54dfb314e761351c0cfa8098b78d848405ce703
anykeynl <the.anykey@gmail.com>
2024-03-25 09:48:56 +0100

    v1

---
+++ TF/SingleVM-CloudInit_v2/vars.tf
@@ -0,0 +1,20 @@
+
+variable datacenter {default = "oci-w01dc"}
+variable datastore {default = "vsanDatastore"}
+variable cluster {default = "oci01-w01-consolidated01"}
+
+variable baseTemplate {default = "TemplateUbuntu2204ci"}
+variable vmName {default="VMUG-demo"}
+variable vmCPU {default = 2}
+variable vmMemory {default =8096}
+
+variable vmFolder {default = "vmugnl"}
+variable vmResourcePool {default = "vmugnl"}
+
+variable vmnetwork {default = "VLAN-2002-HCXDemo"}
+variable vmIP {default = "10.100.12."}
+variable vmNetmask {default = 24}
+variable vmGateway {default = "10.100.12.1"}
+variable vmDNS {default = "10.100.12.1"}
+
+
---
+++ TF/SingleVM-CloudInit_v2/vm.tf
@@ -0,0 +1,32 @@
+resource "vsphere_virtual_machine" "vm" {
+  count = 5
+  name             = "${var.vmName}-${count.index+1}"
+  datastore_id     = data.vsphere_datastore.datastore.id
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  folder           = "Richard"
+
+  num_cpus = var.vmCPU
+  memory   = var.vmMemory
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label = "disk0"
+    size  = data.vsphere_virtual_machine.template.disks.0.size
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
+
+    extra_config = {
+      "guestinfo.metadata"          = base64encode(replace(file("${path.module}/metadata.yaml"), "#MYIP#", "${var.vmIP}${count.index+51}"))
+      "guestinfo.metadata.encoding" = "base64"
+      "guestinfo.userdata"          = base64encode(file("${path.module}/userdata.yaml"))
+      "guestinfo.userdata.encoding" = "base64"
+  }
+}
